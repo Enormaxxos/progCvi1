93781361e83c8a7581c46b052075b9409f28aa67
Enormax <petrpelikan32560@gmail.com>
2022-11-10 23:24:26 +0100

    autopep8 pretty print

--- zapoctovy programy/MatrixClass.py
+++ zapoctovy programy/MatrixClass.py
@@ -1,5 +1,5 @@
 from fractions import Fraction
-from math import floor,ceil
+from math import floor, ceil
 
 # TODO:
 #     -rank
@@ -8,9 +8,10 @@ from math import floor,ceil
 #     -homogeneous solutions..?
 #     -non-homogeneous solutions..? (requires Vector class / 1 x self._m matrix)
 
+
 class Matrix:
 
-    def __init__(self,data):
+    def __init__(self, data):
         self.matrix = []
         self._m = len(data)
         self._n = len(data[0])
@@ -19,10 +20,10 @@ class Matrix:
         for i in range(len(data)):
             self.matrix.append([])
             self.matrix[i] = data[i][:]
-   
-    def toString(self): 
 
-        def centerText(text,charCount):
+    def toString(self):
+
+        def centerText(text, charCount):
             textLen = len(text)
             beforeSpacesCount = floor((charCount - textLen)/2)
             afterSpacesCount = ceil((charCount - textLen)/2)
@@ -31,32 +32,33 @@ class Matrix:
         final = f"\n-- Matrix --\n"
 
         flatMatrix = [str(unit) for row in self.matrix for unit in row]
-        allUnitCharCount = len(max(flatMatrix,key=len))
+        allUnitCharCount = len(max(flatMatrix, key=len))
 
         for i in range(self._m):
             row = "| "
             for j in range(self._n):
-                row += centerText(str(self.matrix[i][j]), allUnitCharCount) + " "
+                row += centerText(str(self.matrix[i][j]),
+                                  allUnitCharCount) + " "
 
             if i == 0 and self.transposed:
                 final += row[:-1] + f" |T\n"
             else:
                 final += row[:-1] + f" |\n"
-            
 
         return final[:-1]
 
-    def __getitem__(self,indexTuple):# MATRIX[i,j] - tuple indexing
-        i,j = indexTuple
+    def __getitem__(self, indexTuple):  # MATRIX[i,j] - tuple indexing
+        i, j = indexTuple
         return self.matrix[i-1][j-1]
 
     # ----MATH OPERATIONS----
 
-    def _add(self,other,self_assign=False):
+    def _add(self, other, self_assign=False):
         if type(other) != Matrix:
             raise Exception("You can only add another matrix to this matrix.")
         if self._n != other._n or self._m != other._m:
-            raise Exception("Matrix addition is only defined for two matrices of the same size.")
+            raise Exception(
+                "Matrix addition is only defined for two matrices of the same size.")
 
         if self_assign:
             for row in range(self._m):
@@ -69,13 +71,14 @@ class Matrix:
                 final.append([])
 
                 for col in range(self._n):
-                    final.append(self.matrix[row][col] + other.matrix[row][col])
+                    final.append(self.matrix[row]
+                                 [col] + other.matrix[row][col])
             return final
 
-    def __add__(self,other): # MATRIX+MATRIX
+    def __add__(self, other):  # MATRIX+MATRIX
         return self._add(other)
 
-    def _constantMult(self,val):
+    def _constantMult(self, val):
         newMatrix = []
 
         for i in range(len(self.matrix)):
@@ -85,23 +88,24 @@ class Matrix:
 
         return Matrix(newMatrix)
 
-    def _matrixMult(self,other):
+    def _matrixMult(self, other):
 
         final = []
 
         if self._n != other._m:
-            raise Exception("Left matrices' column count doesn't equal right matrices' row count. Matrix multiplication is not defined")
-        
+            raise Exception(
+                "Left matrices' column count doesn't equal right matrices' row count. Matrix multiplication is not defined")
+
         for i in range(self._m):
             final.append([])
             for j in range(other._n):
                 final[i].append(0)
                 for k in range(self._n):
                     final[i][j] += self.matrix[i][k] * other.matrix[k][j]
-        
+
         return Matrix(final)
 
-    def __mul__(self,val): # n*MATRIX / MATRIX*MATRIX
+    def __mul__(self, val):  # n*MATRIX / MATRIX*MATRIX
         if type(val) == int or type(val) == float:
             return self._constantMult(Fraction(val).limit_denominator())
         if type(val) == Matrix:
@@ -123,16 +127,17 @@ class Matrix:
 
         return newMatrix
 
-    def __invert__(self): # tilde(~)MATRIX - calls _transposed - Transposes Matrix
+    def __invert__(self):  # tilde(~)MATRIX - calls _transposed - Transposes Matrix
         return self._transposed()
 
     # ----CONSTRUCTORS----
 
     @staticmethod
-    def fromString(matrixString,rowSplitChar="@",colSplitChar="&"): # DEFAULTS - rowSplitChar = "@" colSplitChar = "&"
+    # DEFAULTS - rowSplitChar = "@" colSplitChar = "&"
+    def fromString(matrixString, rowSplitChar="@", colSplitChar="&"):
 
         finalList = []
-    
+
         rows = matrixString.split(rowSplitChar)
         n = len(rows[0].split(colSplitChar))
 
@@ -142,7 +147,8 @@ class Matrix:
             if len(units) != n:
                 raise Exception("All rows must have the same number of units.")
 
-            finalList.append([Fraction(str(unit)).limit_denominator() for unit in units])
+            finalList.append([Fraction(str(unit)).limit_denominator()
+                             for unit in units])
 
         return Matrix(finalList)
 
@@ -154,7 +160,8 @@ class Matrix:
             if len(lst) != n:
                 raise Exception("All rows must have the same number of units.")
 
-            finalList.append([Fraction(str(unit)).limit_denominator() for unit in lst])
+            finalList.append(
+                [Fraction(str(unit)).limit_denominator() for unit in lst])
 
         return Matrix(finalList)
 
@@ -163,15 +170,16 @@ class Matrix:
         n = len(matrixList[0])
 
         for row in matrixList:
-            
+
             if len(row) != n:
                 raise Exception("All rows must have the same number of units.")
 
-            finalList.append([Fraction(str(unit)).limit_denominator() for unit in row])
+            finalList.append(
+                [Fraction(str(unit)).limit_denominator() for unit in row])
 
         return Matrix(finalList)
 
-    def fromInput(): # unlike other constructors requires user input
+    def fromInput():  # unlike other constructors requires user input
         print("Use ampersand (&) as a unit separator between columns. \nWhen you finish inputting row, hit ENTER. \nWhen you finish inputting matrix, leave row empty and hit ENTER.")
         colSeparator = '&'
 
@@ -191,11 +199,10 @@ class Matrix:
 
             if len(rowSplit) != n:
                 raise Exception("All rows must have the same number of units.")
-            
+
             finalList.append([Fraction(unit) for unit in rowSplit])
 
         return Matrix(finalList)
-
 
 
 # a = Matrix.fromLists([16/3,50],[40,48])
