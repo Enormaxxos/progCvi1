33f71baa0bdad57820e9ea152ae1fb68ec7983a4
Enormax <petrpelikan32560@gmail.com>
2022-11-10 17:38:18 +0100

    init commit

--- 7/07_ticha_posta.py
+++ 7/07_ticha_posta.py
@@ -1,14 +1,38 @@
+class Speaker:
+    def __init__(self,name):
+        self.name = name
 
+    def say(self,message):
+        return f"{self.name}: {message}"
+
+class LoudSpeaker(Speaker):
+    def say(self,message):
+        return f"{self.name}: {message.upper()}"
+
+class SilentSpeaker(Speaker):
+    def say(self,message):
+        return f"{self.name}: {message.lower()}"
 
 
 def inputHandler(stringInput):
     splitIndex = stringInput.index(";")
     message = stringInput[:splitIndex]
-    remainder = stringInput[splitIndex+1:]
+    peopleDict = eval(stringInput[splitIndex+1:])
 
-    print(f"message = {message}")
-    print(f"remainder = {remainder}")
-    return True
+    people = []
+
+    sortedKeys = sorted(peopleDict.keys())
+
+    for key in sortedKeys:
+        if peopleDict[key] == 'normální':
+            people.append(Speaker(key))
+        elif peopleDict[key] == 'tichošlápek':
+            people.append(SilentSpeaker(key))
+        else:
+            people.append(LoudSpeaker(key))
+
+    for human in people:
+        print(human.say(message))
 
 
-print(inputHandler("Někdo Klepe!;{'Anna': 'normální', 'Petr': 'tichošlápek', 'Marie': 'řvoun'}"))
+inputHandler(input())
