801718117ee33798dbf26a1ee8ddc128a54e982f
Enormax <petrpelikan32560@gmail.com>
2022-11-10 23:02:54 +0100

    added transposition, added tuple indexing, added fromInput() constructor

--- zapoctovy programy/MatrixClass.py
+++ zapoctovy programy/MatrixClass.py
@@ -5,14 +5,15 @@ class Matrix:
 
     def __init__(self,data):
         self.matrix = []
-        self.m = len(data)
-        self.n = len(data[0])
+        self._m = len(data)
+        self._n = len(data[0])
+        self.transposed = False
 
         for i in range(len(data)):
             self.matrix.append([])
             self.matrix[i] = data[i][:]
    
-    def toString(self):
+    def toString(self): 
 
         def centerText(text,charCount):
             textLen = len(text)
@@ -25,38 +26,46 @@ class Matrix:
         flatMatrix = [str(unit) for row in self.matrix for unit in row]
         allUnitCharCount = len(max(flatMatrix,key=len))
 
-        for i in range(self.m):
+        for i in range(self._m):
             row = "| "
-            for j in range(self.n):
+            for j in range(self._n):
                 row += centerText(str(self.matrix[i][j]), allUnitCharCount) + " "
 
-            final += row[:-1] + " |\n"
+            if i == 0 and self.transposed:
+                final += row[:-1] + f" |T\n"
+            else:
+                final += row[:-1] + f" |\n"
+            
 
         return final[:-1]
 
+    def __getitem__(self,indexTuple):# MATRIX[i{1,2,...,m},j{1,2,...,n}]
+        i,j = indexTuple
+        return self.matrix[i-1][j-1]
+
     # ----MATH OPERATIONS----
 
-    def add(self,other,self_assign=True):
+    def add(self,other,self_assign=False):
         if type(other) != Matrix:
             raise Exception("You can only add another matrix to this matrix.")
-        if self.n != other.n or self.m != other.m:
+        if self._n != other._n or self._m != other._m:
             raise Exception("Matrix addition is only defined for two matrices of the same size.")
 
         if self_assign:
-            for row in range(self.m):
-                for col in range(self.n):
+            for row in range(self._m):
+                for col in range(self._n):
                     self.matrix[row][col] += other.matrix[row][col]
             return True
         else:
             final = []
-            for row in range(self.m):
+            for row in range(self._m):
                 final.append([])
 
-                for col in range(self.n):
+                for col in range(self._n):
                     final.append(self.matrix[row][col] + other.matrix[row][col])
             return final
 
-    def __add__(self,other):
+    def __add__(self,other): # MATRIX+MATRIX
         return self.add(other)
 
     def _constantMult(self,val):
@@ -73,28 +82,48 @@ class Matrix:
 
         final = []
 
-        if self.n != other.m:
+        if self._n != other._m:
             raise Exception("Left matrices' column count doesn't equal right matrices' row count. Matrix multiplication is not defined")
         
-        for i in range(self.m):
+        for i in range(self._m):
             final.append([])
-            for j in range(other.n):
+            for j in range(other._n):
                 final[i].append(0)
-                for k in range(self.n):
+                for k in range(self._n):
                     final[i][j] += self.matrix[i][k] * other.matrix[k][j]
         
         return Matrix(final)
 
-    def __mul__(self,val):
-        if type(val) == int:
-            return self._constantMult(val)
+    def __mul__(self,val): # 8*MATRIX / MATRIX*MATRIX
+        if type(val) == int or type(val) == float:
+            return self._constantMult(Fraction(val).limit_denominator())
         if type(val) == Matrix:
             print(f"others[1][1]=", val.matrix[0][0])
             return self._matrixMult(val)
 
+    def _transposed(self):
+
+        newMatrixList = []
+        self.newN = self._m
+        self.newM = self._n
+
+        for i in range(self.newM):
+            newMatrixList.append([])
+            for j in range(self.newN):
+                newMatrixList[i].append(self.matrix[j][i])
+
+        newMatrix = Matrix(newMatrixList)
+        newMatrix.transposed = not self.transposed
+
+        return newMatrix
+
+    def __invert__(self): # tilde(~)MATRIX - calls _transposed - Transposes Matrix
+        return self._transposed()
+
     # ----CONSTRUCTORS----
+
     @staticmethod
-    def fromString(matrixString,rowSplitChar="@",colSplitChar="&"): # rowSplitChar = "@" colSplitChar = "&"
+    def fromString(matrixString,rowSplitChar="@",colSplitChar="&"): # DEFAULTS - rowSplitChar = "@" colSplitChar = "&"
 
         finalList = []
     
@@ -136,16 +165,45 @@ class Matrix:
 
         return Matrix(finalList)
 
-a = Matrix.fromLists([16/3,40],[40,48])
-b = Matrix.from2DList([[4,5],[3,5]])
+    def fromInput(): # unlike other constructors requires user input
+        print("Use ampersand (&) as a unit separator between columns. \nWhen you finish inputting row, hit ENTER. \nWhen you finish inputting matrix, leave row empty and hit ENTER.")
+        colSeparator = '&'
 
-input("_____________")
+        finalList = []
 
-print("a.toString()",a.toString())
-print("b.toString()",b.toString())
+        row = input()
+        rowSplit = row.split(colSeparator)
+        n = len(rowSplit)
+        finalList.append([Fraction(unit) for unit in rowSplit])
+
+        while row != "":
+            row = input()
+            rowSplit = row.split(colSeparator)
+
+            if row == "":
+                break
+
+            if len(rowSplit) != n:
+                raise Exception("All rows must have the same number of units.")
+            
+            finalList.append([Fraction(unit) for unit in rowSplit])
+
+        return Matrix(finalList)
 
 
-input("_____________")
 
-print("(a*b).toString()",(a*b).toString())
-print("(b*a).toString()",(b*a).toString())
+# a = Matrix.fromLists([16/3,50],[40,48])
+# b = Matrix.from2DList([[4,5],[3,5]])
+
+# input("_____________")
+
+# print("a.toString()",a.toString())
+# print("b.toString()",b.toString())
+
+
+# input("_____________")
+
+# i = int(input("i="))
+# j = int(input("j="))
+
+# print(f"a[{i},{j}]", a[i,j])
