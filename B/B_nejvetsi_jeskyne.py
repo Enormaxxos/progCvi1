9f200e8edc2aa3127da7344607d6161c1f6bcdb4
Enormax <petrpelikan32560@gmail.com>
2022-11-15 16:31:41 +0100

    newest

--- B/B_nejvetsi_jeskyne.py
+++ B/B_nejvetsi_jeskyne.py
@@ -1,9 +1,10 @@
 import math
+import sys
 
-directions = [(1,0),(0,1),(-1,0),(0,-1)]
-mapS = 0
+directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 destroyedTiles = []
 dots = []
+
 
 def biggestCave(map):
     final = dict()
@@ -11,7 +12,7 @@ def biggestCave(map):
     dots = findAllPoints(map)
     # print(dots)
 
-    def recursiveCave(map,pos):
+    def recursiveCave(map, pos):
         dots["notDone"].remove(pos)
         dots["done"].append(pos)
         destroyedTiles.append(pos)
@@ -19,34 +20,37 @@ def biggestCave(map):
         possibleMoves = []
 
         for i in range(len(dots["notDone"])):
-            if math.sqrt((dots["notDone"][i][0]-pos[0])**2 + (dots["notDone"][i][1]-pos[1])**2) == 1:
-                possibleMoves.append(dots["notDone"][i])
-        
+            for d in directions:
+                if pos[0] + d[0] == dots["notDone"][i][0] and pos[1] + d[1] == dots["notDone"][i][1]:
+                    possibleMoves.append(dots["notDone"][i])
+            # if math.sqrt((dots["notDone"][i][0]-pos[0])**2 + (dots["notDone"][i][1]-pos[1])**2) == 1:
+            #     possibleMoves.append(dots["notDone"][i])
+
         # print(f"On pos={pos}, possibleMoves are {possibleMoves}")
 
         for posMove in possibleMoves:
             if posMove not in dots["done"]:
-                recursiveCave(map,posMove)
+                recursiveCave(map, posMove)
 
     while len(dots["notDone"]) > 0:
-        recursiveCave(map,dots["notDone"][0])
+        recursiveCave(map, dots["notDone"][0])
 
-        avgX,avgY = 0,0
+        avgX, avgY = 0, 0
         for tile in destroyedTiles:
             avgX += tile[0]
             avgY += tile[1]
         avgX //= len(destroyedTiles)
         avgY //= len(destroyedTiles)
 
-        final[len(destroyedTiles)] = (avgX,avgY)
+        final[len(destroyedTiles)] = (avgX, avgY)
 
         destroyedTiles.clear()
 
-    print(f"vsechny jeskyne = {final}")
+    # print(f"vsechny jeskyne = {final}")
+
+    return max(final), final[max(final)][0], final[max(final)][1]
 
 
-    return max(final),final[max(final)][0],final[max(final)][1]
-    
 def findAllPoints(map):
     ptsList = dict()
     ptsList["notDone"] = []
@@ -55,34 +59,18 @@ def findAllPoints(map):
     for i in range(len(map)):
         for j in range(len(map[i])):
             if map[i][j] == ".":
-                ptsList["notDone"].append((i,j))
+                ptsList["notDone"].append((i, j))
 
     return ptsList
 
+
 def inputHandler():
     final = []
-    line = input()
 
-    mapS = len(line)
-    mapR = 0
-
-    while len(line) > 0:  
-        final.append(line)
-        mapR += 1
-        line = input()
-
-    # print("--MAP--")
-    # print(final)
-    # print("-------")
-    # print(f"mapS={mapS}")
-    # print(f"mapR={mapR}")
+    for line in sys.stdin:
+        final.append(line.strip())
 
     return biggestCave(final)
 
-printed = inputHandler()
-printedString = ""
 
-for unit in printed:
-    printedString += str(unit) + " "
-
-print(printedString[:-1])
+print(*inputHandler())
